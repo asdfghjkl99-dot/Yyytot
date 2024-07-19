fd2392e4b9a381a9214e3917ca0676cc6b6dea35
MRAADGH <MrSJGDD@gmail.com>
2024-07-19 19:24:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -397,17 +397,33 @@ bot.on('callback_query', (query) => {
 
 
 // مثال على كيفية إرسال أزرار قائمة 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// ... (باقي المتطلبات)
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://SJGGDD:MaySsonu0@cluster0.gqfh8z3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://SJGGDD:<MaySsonu0>@cluster0.gqfh8z3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-  tls: true,
-  tlsAllowInvalidCertificates: true // استخدم هذا الخيار فقط في بيئة التطوير
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // تعريف المتغيرات العامة
 let userPoints = new Map();
